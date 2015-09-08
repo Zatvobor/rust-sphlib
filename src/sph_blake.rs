e549140c1edeee2caeca929a2e813ad0e03f8000
Zatvobor <a.zatvornitskiy@gmail.com>
2015-09-08 15:57:10 +0300

    Raw structure for Blake algo (next step will be generic Trait for particular type), #1

--- src/sph_blake.rs
+++ src/sph_blake.rs
@@ -1,53 +1,43 @@
 #![allow(non_camel_case_types)]
 
 extern crate libc;
-use libc::{uint32_t, int32_t, uint64_t, int64_t, c_uchar, size_t, c_void, c_uint};
+use libc::{uint32_t, uint64_t, c_uchar, size_t, c_void, c_uint};
 use utils::{to_void_raw_ctx, to_void_raw_data, to_void_raw_dest};
-
-pub type sph_u32 = uint32_t;
-pub type sph_s32 = int32_t;
 
 #[repr(C)]
 #[allow(non_snake_case)]
 pub struct BlakeSmallContext {
     pub buf: [c_uchar; 64usize],
     pub ptr: size_t,
-    pub H: [sph_u32; 8usize],
-    pub S: [sph_u32; 4usize],
-    pub T0: sph_u32,
-    pub T1: sph_u32,
+    pub H: [uint32_t; 8usize],
+    pub S: [uint32_t; 4usize],
+    pub T0: uint32_t,
+    pub T1: uint32_t,
 }
 
 impl ::std::default::Default for BlakeSmallContext {
     fn default() -> Self { unsafe { ::std::mem::zeroed() } }
 }
 
-pub type sph_blake_small_context = BlakeSmallContext;
-pub type sph_blake224_context = sph_blake_small_context;
-pub type sph_blake256_context = sph_blake_small_context;
-
-pub type sph_u64 = uint64_t;
-pub type sph_s64 = int64_t;
-
 #[repr(C)]
 #[allow(non_snake_case)]
 pub struct BlakeBigContext {
     pub buf: [c_uchar; 128usize],
     pub ptr: size_t,
-    pub H: [sph_u64; 8usize],
-    pub S: [sph_u64; 4usize],
-    pub T0: sph_u64,
-    pub T1: sph_u64,
+    pub H: [uint64_t; 8usize],
+    pub S: [uint64_t; 4usize],
+    pub T0: uint64_t,
+    pub T1: uint64_t,
 }
 
 impl ::std::default::Default for BlakeBigContext {
     fn default() -> Self { unsafe { ::std::mem::zeroed() } }
 }
 
-pub type sph_blake_big_context = BlakeBigContext;
-pub type sph_blake384_context = sph_blake_big_context;
-pub type sph_blake512_context = sph_blake_big_context;
-
+pub type blake224 = [u8;28];
+pub type blake256 = [u8;32];
+pub type blake384 = [u8;48];
+pub type blake512 = [u8;64];
 
 extern {
     pub fn sph_blake224_init(cc: *mut c_void) -> ();
@@ -71,66 +61,115 @@ extern {
     pub fn sph_blake512_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
 }
 
-pub fn blake224(data: &str) -> [u8;28] {
-    let mut dest: [u8;28] = [0;28];
-    unsafe {
-        let mut cc      = BlakeSmallContext::default();
-        let void_raw_cc = to_void_raw_ctx(&mut cc);
-        sph_blake224_init(void_raw_cc);
 
-        let (void_raw_data, len) = to_void_raw_data(data);
-        sph_blake224(void_raw_cc, void_raw_data, len);
+pub fn blake224_init(cc: &mut BlakeSmallContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_blake224_init(void_raw_cc) };
+}
 
-        let void_raw_dest = to_void_raw_dest(&mut dest);
-        sph_blake224_close(void_raw_cc, void_raw_dest);
-    }
+pub fn blake224_load(cc: &mut BlakeSmallContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_blake224(void_raw_cc, void_raw_data, len) };
+}
+
+pub fn blake224_close(cc: &mut BlakeSmallContext, dest: &mut blake224) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_blake224_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn blake224_init_load_close(data: &str) -> blake224 {
+    let mut dest: blake224 = [0;28];
+    let mut cc             = BlakeSmallContext::default();
+
+    blake224_init(&mut cc);
+    blake224_load(&mut cc, data);
+    blake224_close(&mut cc, &mut dest);
+
     return dest;
 }
 
-pub fn blake256(data: &str) -> [u8;32] {
-    let mut dest: [u8;32] = [0;32];
-    unsafe {
-        let mut cc      = BlakeSmallContext::default();
-        let void_raw_cc = to_void_raw_ctx(&mut cc);
-        sph_blake256_init(void_raw_cc);
+pub fn blake256_init(cc: &mut BlakeSmallContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_blake256_init(void_raw_cc) };
+}
 
-        let (void_raw_data, len) = to_void_raw_data(data);
-        sph_blake256(void_raw_cc, void_raw_data, len);
+pub fn blake256_load(cc: &mut BlakeSmallContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_blake256(void_raw_cc, void_raw_data, len) };
+}
 
-        let void_raw_dest = to_void_raw_dest(&mut dest);
-        sph_blake256_close(void_raw_cc, void_raw_dest);
-    }
+pub fn blake256_close(cc: &mut BlakeSmallContext, dest: &mut blake256) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_blake256_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn blake256_init_load_close(data: &str) -> blake256 {
+    let mut dest: blake256 = [0;32];
+    let mut cc             = BlakeSmallContext::default();
+
+    blake256_init(&mut cc);
+    blake256_load(&mut cc, data);
+    blake256_close(&mut cc, &mut dest);
+
     return dest;
 }
 
-pub fn blake384(data: &str) -> [u8;48] {
-    let mut dest: [u8;48] = [0;48];
-    unsafe {
-        let mut cc      = BlakeBigContext::default();
-        let void_raw_cc = to_void_raw_ctx(&mut cc);
-        sph_blake384_init(void_raw_cc);
+pub fn blake384_init(cc: &mut BlakeBigContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_blake384_init(void_raw_cc) };
+}
 
-        let (void_raw_data, len) = to_void_raw_data(data);
-        sph_blake384(void_raw_cc, void_raw_data, len);
+pub fn blake384_load(cc: &mut BlakeBigContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_blake384(void_raw_cc, void_raw_data, len) };
+}
 
-        let void_raw_dest = to_void_raw_dest(&mut dest);
-        sph_blake384_close(void_raw_cc, void_raw_dest);
-    }
+pub fn blake384_close(cc: &mut BlakeBigContext, dest: &mut blake384) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_blake384_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn blake384_init_load_close(data: &str) -> blake384 {
+    let mut dest: blake384 = [0;48];
+    let mut cc             = BlakeBigContext::default();
+
+    blake384_init(&mut cc);
+    blake384_load(&mut cc, data);
+    blake384_close(&mut cc, &mut dest);
+
     return dest;
 }
 
-pub fn blake512(data: &str) -> [u8;64] {
-    let mut dest: [u8;64] = [0;64];
-    unsafe {
-        let mut cc      = BlakeBigContext::default();
-        let void_raw_cc = to_void_raw_ctx(&mut cc);
-        sph_blake512_init(void_raw_cc);
+pub fn blake512_init(cc: &mut BlakeBigContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_blake512_init(void_raw_cc) };
+}
 
-        let (void_raw_data, len) = to_void_raw_data(data);
-        sph_blake512(void_raw_cc, void_raw_data, len);
+pub fn blake512_load(cc: &mut BlakeBigContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_blake512(void_raw_cc, void_raw_data, len) };
+}
 
-        let void_raw_dest = to_void_raw_dest(&mut dest);
-        sph_blake512_close(void_raw_cc, void_raw_dest);
-    }
+pub fn blake512_close(cc: &mut BlakeBigContext, dest: &mut blake512) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_blake512_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn blake512_init_load_close(data: &str) -> blake512 {
+    let mut dest: blake512 = [0;64];
+    let mut cc             = BlakeBigContext::default();
+
+    blake512_init(&mut cc);
+    blake512_load(&mut cc, data);
+    blake512_close(&mut cc, &mut dest);
+
     return dest;
 }
--- tests/sph_blake_test.rs
+++ tests/sph_blake_test.rs
@@ -6,28 +6,28 @@ use sphlib::{sph_blake, utils};
 
 #[test]
 fn will_be_224_hash() {
-    let dest   = sph_blake::blake224("");
+    let dest   = sph_blake::blake224_init_load_close("");
     let actual = utils::to_hex_hash(&dest);
     assert_eq!("7dc5313b1c04512a174bd6503b89607aecbee0903d40a8a569c94eed", actual.to_string());
 }
 
 #[test]
 fn will_be_256_hash() {
-    let dest   = sph_blake::blake256("");
+    let dest   = sph_blake::blake256_init_load_close("");
     let actual = utils::to_hex_hash(&dest);
     assert_eq!("716f6e863f744b9ac22c97ec7b76ea5f5908bc5b2f67c61510bfc4751384ea7a", actual.to_string());
 }
 
 #[test]
 fn will_be_384_hash() {
-    let dest   = sph_blake::blake384("");
+    let dest   = sph_blake::blake384_init_load_close("");
     let actual = utils::to_hex_hash(&dest);
     assert_eq!("c6cbd89c926ab525c242e6621f2f5fa73aa4afe3d9e24aed727faaadd6af38b620bdb623dd2b4788b1c8086984af8706", actual.to_string());
 }
 
 #[test]
 fn will_be_512_hash() {
-    let dest   = sph_blake::blake512("");
+    let dest   = sph_blake::blake512_init_load_close("");
     let actual = utils::to_hex_hash(&dest);
     assert_eq!("a8cfbbd73726062df0c6864dda65defe58ef0cc52a5625090fa17601e1eecd1b628e94f396ae402a00acc9eab77b4d4c2e852aaaa25a636d80af3fc7913ef5b8", actual.to_string());
 }
