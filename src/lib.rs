92adae7ef8f91d21c3036abb8c0f9661994f3145
Zatvobor <a.zatvornitskiy@gmail.com>
2015-09-08 20:46:34 +0300

    First class reference implementation for JH algo, #10

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod sph_blake;
 pub mod sph_bmw;
 pub mod sph_cubehash;
 pub mod sph_echo;
+pub mod sph_jh;
--- tests/tests.rs
+++ tests/tests.rs
@@ -4,3 +4,4 @@ mod sph_blake_test;
 mod sph_bmw_test;
 mod sph_cubehash_test;
 mod sph_echo_test;
+mod sph_jh_test;
