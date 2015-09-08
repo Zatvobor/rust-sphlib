bc990f41ebe335470065dd4303a919cf8f66b702
Zatvobor <a.zatvornitskiy@gmail.com>
2015-09-08 20:22:44 +0300

    First class reference implementation for Echo algo, #4

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod utils;
 pub mod sph_blake;
 pub mod sph_bmw;
 pub mod sph_cubehash;
+pub mod sph_echo;
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,3 +3,4 @@ extern crate sphlib;
 mod sph_blake_test;
 mod sph_bmw_test;
 mod sph_cubehash_test;
+mod sph_echo_test;
