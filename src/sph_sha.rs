1e7c7f40faaa620b7a84c5069d72cdb193d7af65
Zatvobor <a.zatvornitskiy@gmail.com>
2015-10-02 19:51:38 +0300

    First class reference implementation for SHA224/256/384/512 algos, closes #27

--- src/sph_sha.rs
+++ src/sph_sha.rs
@@ -17,8 +17,36 @@ impl ::std::default::Default for ShaSmallContext {
     fn default() -> Self { unsafe { zeroed() } }
 }
 
-pub type sha0 = [u8;20];
-pub type sha1 = [u8;20];
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct ShaMediumContext {
+    pub buf: [c_uchar; 64usize],
+    pub val: [uint32_t; 8usize],
+    pub count: uint64_t,
+}
+
+impl ::std::default::Default for ShaMediumContext {
+    fn default() -> Self { unsafe { zeroed() } }
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct ShaBigContext {
+    pub buf: [c_uchar; 128usize],
+    pub val: [uint64_t; 8usize],
+    pub count: uint64_t,
+}
+
+impl ::std::default::Default for ShaBigContext {
+    fn default() -> Self { unsafe { zeroed() } }
+}
+
+pub type sha0   = [u8;20];
+pub type sha1   = [u8;20];
+pub type sha224 = [u8;28];
+pub type sha256 = [u8;32];
+pub type sha384 = [u8;48];
+pub type sha512 = [u8;64];
 
 extern {
     pub fn sph_sha0_init(cc: *mut c_void) -> ();
@@ -32,6 +60,26 @@ extern {
     pub fn sph_sha1_close(cc: *mut c_void, dst: *mut c_void) -> ();
     pub fn sph_sha1_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
     pub fn sph_sha1_comp(msg: *mut uint32_t, val: *mut uint32_t) -> ();
+
+    pub fn sph_sha224_init(cc: *mut c_void) -> ();
+    pub fn sph_sha224(cc: *mut c_void, data: *const c_void, len: size_t) -> ();
+    pub fn sph_sha224_close(cc: *mut c_void, dst: *mut c_void) -> ();
+    pub fn sph_sha224_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
+    pub fn sph_sha224_comp(msg: *mut uint32_t, val: *mut uint32_t) -> ();
+
+    pub fn sph_sha256_init(cc: *mut c_void) -> ();
+    pub fn sph_sha256_close(cc: *mut c_void, dst: *mut c_void) -> ();
+    pub fn sph_sha256_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
+
+    pub fn sph_sha384_init(cc: *mut c_void) -> ();
+    pub fn sph_sha384(cc: *mut c_void, data: *const c_void, len: size_t) -> ();
+    pub fn sph_sha384_close(cc: *mut c_void, dst: *mut c_void) -> ();
+    pub fn sph_sha384_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
+    pub fn sph_sha384_comp(msg: *mut uint64_t, val: *mut uint64_t) -> ();
+
+    pub fn sph_sha512_init(cc: *mut c_void) -> ();
+    pub fn sph_sha512_close(cc: *mut c_void, dst: *mut c_void) -> ();
+    pub fn sph_sha512_addbits_and_close(cc: *mut c_void, ub: c_uint, n: c_uint, dst: *mut c_void) -> ();
 }
 
 
@@ -87,6 +135,118 @@ pub fn sha1_init_load_close(data: &str) -> sha1 {
     sha1_init(&mut cc);
     sha1_load(&mut cc, data);
     sha1_close(&mut cc, &mut dest);
+
+    return dest;
+}
+
+pub fn sha224_init(cc: &mut ShaMediumContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_sha224_init(void_raw_cc) };
+}
+
+pub fn sha224_load(cc: &mut ShaMediumContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_sha224(void_raw_cc, void_raw_data, len) };
+}
+
+pub fn sha224_close(cc: &mut ShaMediumContext, dest: &mut sha224) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_sha224_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn sha224_init_load_close(data: &str) -> sha224 {
+    let mut dest: sha224 = [0;28];
+    let mut cc           = ShaMediumContext::default();
+
+    sha224_init(&mut cc);
+    sha224_load(&mut cc, data);
+    sha224_close(&mut cc, &mut dest);
+
+    return dest;
+}
+
+pub fn sha256_init(cc: &mut ShaMediumContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_sha256_init(void_raw_cc) };
+}
+
+pub fn sha256_load(cc: &mut ShaMediumContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_sha224(void_raw_cc, void_raw_data, len) };
+}
+
+pub fn sha256_close(cc: &mut ShaMediumContext, dest: &mut sha256) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_sha256_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn sha256_init_load_close(data: &str) -> sha256 {
+    let mut dest: sha256 = [0;32];
+    let mut cc           = ShaMediumContext::default();
+
+    sha256_init(&mut cc);
+    sha256_load(&mut cc, data);
+    sha256_close(&mut cc, &mut dest);
+
+    return dest;
+}
+
+pub fn sha384_init(cc: &mut ShaBigContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_sha384_init(void_raw_cc) };
+}
+
+pub fn sha384_load(cc: &mut ShaBigContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_sha384(void_raw_cc, void_raw_data, len) };
+}
+
+pub fn sha384_close(cc: &mut ShaBigContext, dest: &mut sha384) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_sha384_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn sha384_init_load_close(data: &str) -> sha384 {
+    let mut dest: sha384 = [0;48];
+    let mut cc           = ShaBigContext::default();
+
+    sha384_init(&mut cc);
+    sha384_load(&mut cc, data);
+    sha384_close(&mut cc, &mut dest);
+
+    return dest;
+}
+
+pub fn sha512_init(cc: &mut ShaBigContext) {
+    let void_raw_cc = to_void_raw_ctx(cc);
+    unsafe { sph_sha512_init(void_raw_cc) };
+}
+
+pub fn sha512_load(cc: &mut ShaBigContext, data: &str) {
+    let void_raw_cc             = to_void_raw_ctx(cc);
+    let (void_raw_data, len)    = to_void_raw_data(data);
+    unsafe { sph_sha384(void_raw_cc, void_raw_data, len) };
+}
+
+pub fn sha512_close(cc: &mut ShaBigContext, dest: &mut sha512) {
+    let void_raw_cc     = to_void_raw_ctx(cc);
+    let void_raw_dest   = to_void_raw_dest(dest);
+    unsafe { sph_sha512_close(void_raw_cc, void_raw_dest); };
+}
+
+pub fn sha512_init_load_close(data: &str) -> sha512 {
+    let mut dest: sha512 = [0;64];
+    let mut cc              = ShaBigContext::default();
+
+    sha512_init(&mut cc);
+    sha512_load(&mut cc, data);
+    sha512_close(&mut cc, &mut dest);
 
     return dest;
 }
--- tests/sph_sha_test.rs
+++ tests/sph_sha_test.rs
@@ -16,3 +16,31 @@ fn will_be_sha1_hash() {
     let actual = utils::to_hex_hash(&dest);
     assert_eq!("da39a3ee5e6b4b0d3255bfef95601890afd80709", actual.to_string());
 }
+
+#[test]
+fn will_be_sha224_hash() {
+    let dest   = sph_sha::sha224_init_load_close("");
+    let actual = utils::to_hex_hash(&dest);
+    assert_eq!("d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f", actual.to_string());
+}
+
+#[test]
+fn will_be_sha256_hash() {
+    let dest   = sph_sha::sha256_init_load_close("");
+    let actual = utils::to_hex_hash(&dest);
+    assert_eq!("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", actual.to_string());
+}
+
+#[test]
+fn will_be_sha384_hash() {
+    let dest   = sph_sha::sha384_init_load_close("");
+    let actual = utils::to_hex_hash(&dest);
+    assert_eq!("38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b", actual.to_string());
+}
+
+#[test]
+fn will_be_sha512_hash() {
+    let dest   = sph_sha::sha512_init_load_close("");
+    let actual = utils::to_hex_hash(&dest);
+    assert_eq!("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", actual.to_string());
+}
